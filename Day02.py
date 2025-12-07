e494eb533e806ebbd5ce5741bdf37cc25c254d56
TiesMartens <87567401+TiesMartens@users.noreply.github.com>
2025-12-07 23:56:11 +0100

    Greedy implementation Day 2

--- Day02.py
+++ Day02.py
@@ -11,55 +11,62 @@ def part1(input):
     input = input.split(',')
     invalids = 0
 
-    def split_range(n_range):
-        start = n_range[0]
-        end = n_range[1]
-        if len(start) == len(end):
-            return [n_range]
-        else:
-            ranges = []
-            cur = int(start)
-            for l in range(len(start), len(end)+1):
-                ranges.append([str(cur), str(min(10**l - 1, int(end)))])
-                cur = 10**l
-            return ranges
+    def check_nr(x):
+        x_str = str(x)
+        d = 2
+        if len(x_str) % d == 0:
+            n = len(x_str) // d
+            pieces = [x_str[i * n:(i + 1) * n] for i in range(d)]
+            invalid = len(set(pieces)) == 1
+            if invalid:
+                return True
 
+        return False
 
-    def check_n_range(n_range):
+    def check_n_range(r):
         score = 0
-        if len(n_range[0]) % 2 == 1:
-            return score
-        else:
-            start = int(n_range[0])
-            end = int(n_range[1])
-            current = start
-            half = int(len(n_range[0]) / 2)
-
-            if int(start / 10**half) < start - int(start / 10**half) * 10**half:
-                current = (int(start / 10**half) + 1) * 10**half
-
-            while int(current / 10**half) < int(end / 10**half):
-                score += int(current / 10**half) * 10**half + int(current / 10**half)
-                current = (int(current / 10**half) + 1) * 10**half
-
-            if int(current / 10**half) == int(end / 10**half):
-                if int(current / 10**half) <= end - int(end / 10**half) *10**half:
-                    score += int(current / 10**half) * 10**half + int(current / 10**half)
+        for i in range(int(r[0]),int(r[1])+1):
+            if check_nr(i):
+                score += i
         return score
 
     for item in input:
-        n_range = item.split('-')
-        ranges = split_range(n_range)
-        for r in ranges:
-            invalids += check_n_range(r)
+        r = item.split('-')
+        invalids += check_n_range(r)
 
     answer1 = invalids
     return answer1
 
 
 def part2(input):
+    input = input[0]
+    input = input.split(',')
+    invalids = 0
 
-    answer2 = 0
+    def check_nr_all(x):
+        x_str = str(x)
+        for d in range(1, len(x_str)):
+            if len(x_str) % d == 0:
+                n = len(x_str) // d
+                pieces = [x_str[i * d:(i + 1) * d] for i in range(n)]
+                invalid = len(set(pieces)) == 1
+                if invalid:
+                    return True
+
+        return False
+
+    def check_n_range(r):
+        score = 0
+        for i in range(int(r[0]), int(r[1]) + 1):
+            if check_nr_all(i):
+                score += i
+        return score
+
+    for item in input:
+        r = item.split('-')
+        invalids += check_n_range(r)
+
+    answer2 = invalids
     return answer2
 
 result.print_results(part1, part2, file)
