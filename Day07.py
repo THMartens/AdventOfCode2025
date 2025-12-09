df9cf08ddce8e710794cf6aa62c5ae904a1f9744
TiesMartens <87567401+TiesMartens@users.noreply.github.com>
2025-12-09 16:27:28 +0100

    Day 07 part 2

--- Day07.py
+++ Day07.py
@@ -39,30 +39,46 @@ def part1(input):
 
 
 def part2(input):
-    beams = []
-    nr_timelines = 1
-    for i, char in enumerate(input[0]):
+
+    nr_timelines = 0
+
+    def add_splitter(input, splitters, i, cur_row):
+        j = cur_row + 1
+        while j < len(input) - 1:
+            if (j, i-1) in splitters:
+                splitters[(cur_row, i)] = splitters[(j, i-1)]
+                break
+            j += 1
+
+        if j == len(input) - 1:
+            splitters[(cur_row, i)] = 1
+
+        j = cur_row + 1
+        while j < len(input) - 1:
+            if (j, i+1) in splitters:
+                splitters[(cur_row, i)] += splitters[(j, i+1)]
+                break
+            j += 1
+
+        if j == len(input) - 1:
+            splitters[(cur_row, i)] += 1
+
+        return splitters
+
+
+    splitters = {}
+    for r in reversed(range(len(input) - 1)):
+
+        for i, char in enumerate(input[r]):
+            if char == '^' or char == 'S':
+                splitters = add_splitter(input, splitters, i, r)
+
+    for k, char in enumerate(input[0]):
         if char == 'S':
-            beams.append([0, i])
+            nr_timelines = splitters[(0, k)]
             break
 
-    def progress(input, beam, beams,  nr_timelines):
-        if input[beam[0] + 1][beam[1]] == '.':
-            beams.append([beam[0] + 1, beam[1]])
-            return beams, nr_timelines
-
-        if input[beam[0] + 1][beam[1]] == '^':
-            nr_timelines += 1
-            beams.append([beam[0] + 2, beam[1] - 1])
-            beams.append([beam[0] + 2, beam[1] + 1])
-            return beams, nr_timelines
-
-    while len(beams) > 0:
-        beam = beams.pop()
-        if beam[0] < len(input) - 1:
-            beams, nr_timelines = progress(input, beam, beams, nr_timelines)
-
-
+    print(splitters)
     answer2 = nr_timelines
     return answer2
 
